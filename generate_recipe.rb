53dda3ca10a2769a8c27fa4f91ff718ac8da5b7f
Scarlett Clark <sgclark@kde.org>
2016-07-05 17:41:02 -0700

    dd missing dep libattra-devel

--- generate_recipe.rb
+++ generate_recipe.rb
@@ -47,7 +47,7 @@ appimage = Recipe.new
 appimage.name = "peruse"
 appimage.version = '16.04.1'
 #TO_DO do some LD magic here? kdev-tools cmake parser?
-appimage.depends = 'unrar-devel zlib-devel polkit-devel'
+appimage.depends = 'unrar-devel zlib-devel polkit-devel libattr-devel'
 #Needed to add ability to pull in external builds that are simply to old
 #in Centos.
 #appimage.external = 'libarchive,https://github.com/libarchive/libarchive,true,""'
